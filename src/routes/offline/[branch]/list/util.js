b15a2a6c8a6481aeafd7ef7b247987bf1add5484
Mufassir Ahmad Chowdhury <mac22214u@gmail.com>
2024-10-14 19:17:04 +0600

    fixed csv file

--- src/routes/offline/[branch]/list/util.js
+++ src/routes/offline/[branch]/list/util.js
@@ -2,42 +2,30 @@
 
 export function handleExportCSV(registrations) {
     const headers = [
-      'Name', 'Name (English)', 'Father\'s Name', 'Mother\'s Name', 'Institution', 'Class',
-      'Section', 'Class Roll', 'Birth Date', 'Religion', 'Mobile', 'Village', 'Post Office',
-      'Upazila', 'District', 'Present Address', 'Guardian Name', 'Relation', 'Guardian Mobile',
-      'Transaction ID', 'Creation Time'
+      'Serial', 'Institute Type', 'Gender', 'Name', 'Father\'s name', 'Institute',
+      'Class', 'Section', 'Class Roll', 'Mobile', 'Present Address', 'Creation Time', 'Branch'
     ];
   
     const csvContent = [
       headers.join(','),
       ...registrations.map(reg => [
-        reg.name,
-        reg.nameEnglish,
-        reg.fatherName,
-        reg.motherName,
-        reg.institutionType,
-        reg.gender,
-        reg.institution,
-        reg.class,
-        reg.section,
-        reg.classRoll,
-        reg.birthDate,
-        reg.religion,
-        reg.mobile,
-        reg.permanentAddress.village,
-        reg.permanentAddress.postOffice,
-        reg.permanentAddress.upazila,
-        reg.permanentAddress.district,
-        reg.presentAddress,
-        reg.guardianName,
-        reg.relation,
-        reg.guardianMobile,
-        reg.transactionID,
-        reg.creationTime
+    reg.serial,
+    reg.institutionType,
+		reg.gender,
+		reg.name,
+		reg.fatherName,
+		reg.institution,
+		reg.class,
+		reg.section,
+		reg.classRoll,
+		reg.mobile,
+		reg.presentAddress,
+		reg.creationTime,
+        reg.branch,
       ].map(field => `"${field}"`).join(','))
     ].join('\n');
   
-    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-16;' });
     const link = document.createElement("a");
     if (link.download !== undefined) {
       const url = URL.createObjectURL(blob);
